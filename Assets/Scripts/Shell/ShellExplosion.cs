d3fa29b3d005ad0ace2e3c2c1d05fcb8c329cb5c
antonioasaro <antonio_asaro@hotmail.com>
2018-02-24 16:43:59 -0500

    ScriptableObjects - shell explosion

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -4,7 +4,8 @@ public class ShellExplosion : MonoBehaviour
 {
     public LayerMask m_TankMask;
     public ParticleSystem m_ExplosionParticles;       
-    public AudioSource m_ExplosionAudio;              
+    public AudioSource m_ExplosionAudio;    
+	public AudioEvent m_ExplosionAudioEvent;
     public float m_MaxDamage = 100f;                  
     public float m_ExplosionForce = 1000f;            
     public float m_MaxLifeTime = 2f;                  
@@ -34,8 +35,8 @@ public class ShellExplosion : MonoBehaviour
 		}
 		m_ExplosionParticles.transform.parent = null;
 		m_ExplosionParticles.Play ();
-		m_ExplosionAudio.Play ();
-		Destroy (m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+		m_ExplosionAudioEvent.Play (m_ExplosionAudio);		// m_ExplosionAudio.Play ();
+		Destroy (m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
 		Destroy (gameObject);
     }
 
